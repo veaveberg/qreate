dae98fff8a1fe9b9be1677a0ab82faa83d94b700
sasha <sasha@VeaveBook-Pro.local>
2025-05-28 13:32:03 +0400

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/qreate/',  // Ensure this matches your GitHub repository name exactly
-  plugins: [react()]
-})
